3b9e6061d11485a7ce8e322fd135b32d9ecac2c9
Grzegorz Duraj <grzegorzduraj@outlook.com>
2017-05-09 12:57:02 +0200

    Add project files.

---
+++ toturial_call_after_translate/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace toturial_call_after_translate
+{
+    [Activity(Label = "toturial_call_after_translate", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
